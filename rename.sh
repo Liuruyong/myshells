97ca739a39b491964dacfd9edf2d08c2f7a857fa
liuruyong <466374269@qq.com>
2016-11-15 10:29:41 +0800

    rename.sh 文件夹指定为当前文件夹

--- rename.sh
+++ rename.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#目录下文件重命名 把文件名中的特定字符串替换掉.
-#$2 原来的串
-#$3 要替换成的串
-targetStr=$2
-newTargetStr=$3
+#当前目录下文件重命名 把文件名中的特定字符串替换掉.
+#$1 原来的串
+#$2 要替换成的串
+targetStr=$1
+newTargetStr=$2
 
-for filePath in $1/*; do
+for filePath in ./*; do
 	file_name=`basename $filePath`
 	# 判断文件名中是否包含某个字符串
 	if [[ "$file_name" == *"$targetStr"* ]]; then
