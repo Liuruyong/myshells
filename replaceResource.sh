626e498dd01953057a0fc14553331e1c022c4d95
liuruyong <466374269@qq.com>
2017-06-09 13:48:07 +0800

    替换ShareSDK图片

---
+++ replaceResource.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp -a ~/iOS_resource/Icon ~/work/HNNniu/HN-Nniu/Pods/ShareSDK3/ShareSDK/Support/Optional/ShareSDKUI.bundle
+cp ~/iOS_resource/ShareSDKUI_Localizable.strings ~/work/HNNniu/HN-Nniu/Pods/ShareSDK3/ShareSDK/Support/Optional/ShareSDKUI.bundle/zh-Hans.lproj/ShareSDKUI_Localizable.strings
